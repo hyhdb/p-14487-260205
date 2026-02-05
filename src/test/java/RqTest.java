4efd61d06147a6b0891949d0d5b29e80cc3d1eee
mw1126 <kkminwoo@gmail.com>
2026-02-05 15:08:30 +0900

    t11 green

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -96,4 +96,22 @@ public class RqTest {
         String rst = rq.getParam("keywordType", "");
         assertThat(rst).isEqualTo("");
     }
+
+    @Test
+    @DisplayName(value = "명령: 목록?keyword= 일때, rq.getParam(\"keywordType\", \"\") -> \"\"")
+    void t11() {
+
+        Rq rq = new Rq("목록?keyword=");
+        String rst = rq.getParam("keyword", "");
+        assertThat(rst).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName(value = "명령: 목록?keyword 일때, rq.getParam(\"keywordType\", \"\") -> \"\"")
+    void t12() {
+
+        Rq rq = new Rq("목록?keyword");
+        String rst = rq.getParam("keyword", "");
+        assertThat(rst).isEqualTo("");
+    }
 }
