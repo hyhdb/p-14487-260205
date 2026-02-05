9fd249194a78e33406a61219171d5d2625a5bdd2
mw1126 <kkminwoo@gmail.com>
2026-02-05 14:29:34 +0900

    t7&t8 red green

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -69,4 +69,13 @@ public class RqTest {
         int rst = rq.getParamAsInt("page");
         assertThat(rst).isEqualTo(10);
     }
+
+    @Test
+    @DisplayName(value = "명령: 목록?keyword=자바&keywordType=title&page=1 일때, rq.getParam(\"keywordType\") -> \"title\"")
+    void t8() {
+
+        Rq rq = new Rq("목록?keyword=자바&keywordType=title&page=1");
+        String rst = rq.getParam("keywordType");
+        assertThat(rst).isEqualTo("title");
+    }
 }
